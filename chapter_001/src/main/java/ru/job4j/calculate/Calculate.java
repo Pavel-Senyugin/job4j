a8bd2c6fe701b21d12324ceb4f84e29aa58e36d5
Pavel-Senyugin <mister.senyugin@yandex.ru>
2019-02-25 10:08:34 +0300

    1.2. Подключить сборщик Maven[#112348]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -2,9 +2,12 @@ package ru.job4j.colculate;
 
 public class Calculate{
 	
+public String echo(String value){
+	   return String.format("%s %s %s", value, value, value);
+}
 public static void main (String[] args){
-	
-	System.out.println("Hello World");
+	   Calculate calc = new Calculate();
+	   System.out.println(calc.echo("aah"));
 	
 	}	
 	
