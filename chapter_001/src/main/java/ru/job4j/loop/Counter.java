5ab5dc10088024d3cf999f40cc2224896ed75b92
Pavel-Senyugin <mister.senyugin@yandex.ru>
2019-03-19 14:26:12 +0300

    Counter

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -7,29 +7,23 @@ package ru.job4j.loop;
 
 public class Counter {
     public int add(int start, int finish) {
-        start = 0;
-        finish = 10;
         int sum = 0;
         for (int index = start; index <= finish; index++) {
-            if (index % 2 != 0) {
-                continue;
+            if (index % 2 ==0){
+                sum += index;
             }
-            index = (2 + 4 + 6 + 8 + 10);
         }
         return sum;
-
     }
 
     public int add1(int start, int finish) {
-        start = 0;
-        finish = 10;
         int sum = 0;
         for (int index = start; index <= finish; index++) {
             if (index == 5) {
+                sum += index;
                 break;
             }
         }
         return sum;
     }
-
 }
--- chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/CounterTest.java
@@ -11,14 +11,17 @@ public class CounterTest {
     @Test
     public void whenSumEvenNumbersFromOneToTenThenThirty() {
         Counter counter = new Counter();
-        int result = counter.add(0, 10);
-        assertThat(result, is(0));
+        int result = counter.add(1, 10);
+        int expected = 30;
+        assertThat(result, is(expected));
     }
+
     @Test
     public void whenSumEvenNumbersFromOneToTenThenFive() {
         Counter counter = new Counter();
         int result = counter.add1(0, 10);
-        assertThat(result, is(0));
+        int expected = 5;
+        assertThat(result, is(expected));
     }
 
 }
